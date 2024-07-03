c69d15e340c56b9443d220990801af28a44e9279
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-07-02 23:12:36 -0600

    Added code action key

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,13 +2,18 @@ local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
 
-  lsp_zero.default_keymaps({buffer = bufnr})
+	lsp_zero.default_keymaps({buffer = bufnr})
+
+	vim.keymap.set("n", '<leader>.',vim.lsp.buf.code_action)
 
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+  ensure_installed = {
+	"pyright",
+	"denols"
+  },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
