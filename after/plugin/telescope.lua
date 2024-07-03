ae707ba153ca977b82d141c36acee0259ca57e84
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-07-02 22:50:36 -0600

    Some formatting

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -31,5 +31,5 @@ telescope.setup {
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep >") })
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
