835ab41be7538e9d90bb0c40d885170b1f5e406d
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-07-05 19:38:35 -0600

    Added keymap for formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,7 @@ lsp_zero.on_attach(function(client, bufnr)
 	lsp_zero.default_keymaps({buffer = bufnr})
 
 	vim.keymap.set("n", '<leader>.',vim.lsp.buf.code_action)
+	vim.keymap.set("n", '<S-A-f>', vim.lsp.buf.format)
 
 end)
 
