5558636266f990b67fb2c7667a7e965a5b047cab
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-07-08 20:38:43 -0600

    Setup deno and node lsp to triggers initialize them

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,14 +9,27 @@ lsp_zero.on_attach(function(client, bufnr)
 
 end)
 
+local nvim_lsp = require("lspconfig")
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
 	"pyright",
+	"denols"
   },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
+    end,
+    tsserver = function ()
+	    require('lspconfig').tsserver.setup{
+		    single_file_support = false,
+		    root_dir = nvim_lsp.util.root_pattern("package.json")
+	    }
+    end,
+    denols = function()
+	    require('lspconfig').denols.setup{
+		    root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),	
+	    }
     end,
   },
 })
