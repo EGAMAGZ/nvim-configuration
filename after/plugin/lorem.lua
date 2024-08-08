4f5bb3edf51be7856fd91d95b8d80c95a0bbf3d5
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-08-08 01:01:45 -0600

    Created keymap to generate custom number of paragraphs and words

--- after/plugin/lorem.lua
+++ after/plugin/lorem.lua
@@ -1,1 +1,24 @@
-require("lorem").setup()
+require("lorem").setup{
+	sentenceLength = {
+		words_per_sentence = 8,
+		sentences_per_paragraph = 6
+	},
+}
+
+function generate_lorem(message, mode)
+	local number = vim.fn.input(message.." > ")
+
+	if type(number) == "number" and number >= 1 then
+		vim.cmd("Lorem "..number.." "..mode)
+	else
+		vim.print("Please insert a valid number")
+	end
+end
+
+vim.keymap.set('n', '<leader>lp',function()
+	generate_lorem("Number of Paragraphs", "paragraphs")
+end, { desc="Generate N number of paragraphs" })
+
+vim.keymap.set('n', '<leader>lw',function()
+	generate_lorem("Number of Words", "words")
+end, { desc="Generate N number of paragraphs" })
