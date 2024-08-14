01de4bce8722d60afd83204253329af2881c4b95
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-08-14 15:29:37 -0600

    Created keymap to toggle inlay hints

--- after/plugin/inlay-hints.lua
+++ after/plugin/inlay-hints.lua
@@ -2,3 +2,5 @@ require('inlay-hints').setup{
 	commands = { enable = true },
 	autocmd = { enable = true }
 }
+
+vim.keymap.set('n', '<leader>ih', function() vim.cmd[[InlayHintsToggle]] end, { desc="Toggle Inlay hints" })
