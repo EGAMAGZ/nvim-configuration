f0f3900c34a92761f2282c8a9051ce1dd13eca93
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-08-13 22:27:55 -0600

    Succesfully added alternatives ways to change to normalmode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,5 +4,6 @@ vim.keymap.set('n', '<leader>th', "<cmd>horizontal botright terminal<cr>", { des
 vim.keymap.set('n', '<leader>ts', "<cmd>vertical botright terminal<cr>", { desc="Open terminal vertically" })
 
 -- Keymaps for Esc alternatives
-vim.keymap.set({'i','v'}, '<Space-n>', '<Esc>', { desc="Change to normal mode"})
+vim.keymap.set('i', '<leader>nm', function() vim.cmd[[stopinsert]] end, { desc="Change to normal mode"})
+vim.keymap.set('v', '<leader>nm', '<Esc>', { desc="Change to normal mode"})
 
