a97ab697f650573d1a27e427f290fa1bd233792a
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-08-14 00:18:01 -0600

    Added catppuccin's integration of native lsp

--- after/plugin/catpuccin.lua
+++ after/plugin/catpuccin.lua
@@ -64,5 +64,25 @@ require("catppuccin").setup {
 			dim_context = false,
 			alt_background = false,
 		},
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+				ok = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+				ok = { "underline" },
+			},
+			inlay_hints = {
+				background = true,
+			},
+		},
 	}
 }
