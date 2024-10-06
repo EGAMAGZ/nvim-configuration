d1771dc4588141348ee1f1b07ebe6bfa6564aff9
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-10-06 17:31:05 -0600

    Added keymaps to open/close diffview and open file history

--- after/plugin/cmd-line.lua
+++ after/plugin/cmd-line.lua
@@ -1,4 +1,3 @@
 require("fine-cmdline").setup()
 
-vim.keymap.set('n', ':', '<cmd>FineCmdline<CR>', {noremap = true, desc="Open floating command line"})
-vim.keymap.set('n', '<C-k>', '<cmd>FineCmdline<CR>', {noremap = true, desc="Open floating command line"})
+':', '<cmd>FineCmdline<CR>', {noremap = true, desc="Open floating command line"})
---
+++ after/plugin/diffview.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>dvo",vim.cmd.DiffviewOpen, {desc="Open Diff view"})
+vim.keymap.set("n", "<leader>dvc",vim.cmd.DiffviewClose, {desc="Close Diff view"})
+vim.keymap.set("n", "<leader>dvh",vim.cmd.DiffviewFileHistory, {desc="Open File History"})
