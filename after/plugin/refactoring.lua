f3a1fbb96a31e8f261581be6e896340737becf11
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-12-06 10:17:59 -0600

    Added keymaps for refactoring nvim

--- after/plugin/refactoring.lua
+++ after/plugin/refactoring.lua
@@ -1,17 +1,17 @@
 require('refactoring').setup{
 }
-vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end)
-vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end)
+vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end, {desc="Extract function"})
+vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end, {desc="Extract function to file"})
 -- Extract function supports only visual mode
-vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end)
+vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end, {desc="Extract variable"})
 -- Extract variable supports only visual mode
-vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end)
+vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end, {desc="Inline function"})
 -- Inline func supports only normal
-vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end)
+vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end, {desc="Inline variable"})
 -- Inline var supports both normal and visual mode
 
-vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end)
-vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end)
+vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end, {desc="Extract block"})
+vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end, {desc="Extract block to file"})
 
 
 -- You can also use below = true here to to change the position of the printf
@@ -19,15 +19,16 @@ vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('E
 vim.keymap.set(
 	"n",
 	"<leader>rp",
-	function() require('refactoring').debug.printf({below = false}) end
+	function() require('refactoring').debug.printf({below = false}) end,
+	{desc="Debug print"}
 )
 
 -- Print var
 
-vim.keymap.set({"x", "n"}, "<leader>rv", function() require('refactoring').debug.print_var() end)
+vim.keymap.set({"x", "n"}, "<leader>rv", function() require('refactoring').debug.print_var() end, {desc="Print variable"})
 -- Supports both visual and normal mode
 
-vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end)
+vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end, {desc="Cleanup"})
 -- Supports only normal mode
 
 -- load refactoring Telescope extension
@@ -36,5 +37,6 @@ require("telescope").load_extension("refactoring")
 vim.keymap.set(
 	{"n", "x"},
 	"<leader>rr",
-	function() require('telescope').extensions.refactoring.refactors() end
+	function() require('telescope').extensions.refactoring.refactors() end,
+	{desc="Refactors selection"}
 )
