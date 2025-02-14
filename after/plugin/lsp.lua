37812a834e0d5a0da395647cbfa4617654280672
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-02-14 16:02:35 -0600

    Formatted code

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,62 +12,62 @@ local nvim_lsp = require("lspconfig")
 
 require('mason').setup({})
 require('mason-lspconfig').setup {
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-    ts_ls = function ()
-	    require('lspconfig').ts_ls.setup{
-		    single_file_support = false,
-		    root_dir = nvim_lsp.util.root_pattern("package.json"),
-		    -- Inlay hints' settings
-		    settings = {
-			    typescript = {
-				    inlayHints = {
-					    includeInlayParameterNameHints = "all",
-					    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-					    includeInlayFunctionParameterTypeHints = true,
-					    includeInlayVariableTypeHints = true,
-					    includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-					    includeInlayPropertyDeclarationTypeHints = true,
-					    includeInlayFunctionLikeReturnTypeHints = true,
-					    includeInlayEnumMemberValueHints = true,
-				    },
-			    },
-			    javascript = {
-				    inlayHints = {
-					    includeInlayParameterNameHints = "all",
-					    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-					    includeInlayFunctionParameterTypeHints = true,
-					    includeInlayVariableTypeHints = true,
-					    includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-					    includeInlayPropertyDeclarationTypeHints = true,
-					    includeInlayFunctionLikeReturnTypeHints = true,
-					    includeInlayEnumMemberValueHints = true,
-				    },
-			    },
-		    }
-	    }
-    end,
-    denols = function()
-	    require('lspconfig').denols.setup{
-		    root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),	
-		    -- Inlay hints' settings
-		    settings = {
-			    deno = {
-				    inlayHints = {
-					    parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = true },
-					    parameterTypes = { enabled = true },
-					    variableTypes = { enabled = true, suppressWhenTypeMatchesName = true },
-					    propertyDeclarationTypes = { enabled = true },
-					    functionLikeReturnTypes = { enable = true },
-					    enumMemberValues = { enabled = true },
-				    },
-			    }
-		    }
-	    }
-    end,
-  },
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+		ts_ls = function ()
+			require('lspconfig').ts_ls.setup{
+				single_file_support = false,
+				root_dir = nvim_lsp.util.root_pattern("package.json"),
+				-- Inlay hints' settings
+				settings = {
+					typescript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+					javascript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+				}
+			}
+		end,
+		denols = function()
+			require('lspconfig').denols.setup{
+				root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),	
+				-- Inlay hints' settings
+				settings = {
+					deno = {
+						inlayHints = {
+							parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = true },
+							parameterTypes = { enabled = true },
+							variableTypes = { enabled = true, suppressWhenTypeMatchesName = true },
+							propertyDeclarationTypes = { enabled = true },
+							functionLikeReturnTypes = { enable = true },
+							enumMemberValues = { enabled = true },
+						},
+					}
+				}
+			}
+		end,
+	},
 }
 
 nvim_lsp.gleam.setup({})
